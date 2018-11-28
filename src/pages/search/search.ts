182f69f16660ec65008d0a22100cc47086234f5f
abhiutd <abhibits2012@gmail.com>
2018-11-28 16:36:51 -0600

    update search

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Post } from '../../models/post';
 import { Posts } from '../../providers';
+import { User } from '../../models/user';
+import { Users } from '../../providers';
+import { Topic } from '../../models/topic';
+import { Topics } from '../../providers';
 
 @IonicPage()
 @Component({
@@ -12,9 +16,13 @@ import { Posts } from '../../providers';
 export class SearchPage {
 
   currentPosts: Post[] = [];
+  currentUsers: User[] = [];
+  currentTopics: Topic[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public posts: Posts) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public posts: Posts, public users: Users, public topics: Topics) {
     this.currentPosts = []
+    this.currentUsers = []
+    this.currentTopics = []
   }
 
   /**
@@ -39,6 +47,53 @@ export class SearchPage {
     this.navCtrl.push('PostDetailPage', {
       post: post
     });
-  }
+    }
 
+    /**
+    * Perform a service for the proper users.
+    */
+   getUsers(ev) {
+     let val = ev.target.value;
+     if (!val || !val.trim()) {
+       this.currentUsers = [];
+       return;
+     }
+     this.currentUsers = this.users.query({
+       username: val
+     });
+     return;
+   }
+
+   /**
+    * Navigate to the detail page for this user.
+    */
+   openBio(user: User) {
+     this.navCtrl.push('UserDetailPage', {
+       user: user
+     });
+   }
+
+   /**
+    * Perform a service for the proper topics
+    */
+   getTopics(ev) {
+     let val = ev.target.value;
+     if (!val || !val.trim()) {
+       this.currentTopics = [];
+       return;
+     }
+     this.currentTopics = this.topics.query({
+       topic: val
+     });
+     return;
+   }
+
+   /**
+    * Navigate to the detail page for this topic.
+    */
+   openTopic(topic: Topic) {
+     this.navCtrl.push('TopicDetailPage', {
+       topic: topic
+     });
+   }
 }
