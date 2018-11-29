e3888d6fdf3cf1a96d5e87d52707794928f9ba38
abhiutd <abhibits2012@gmail.com>
2018-11-29 02:41:30 -0600

    update

--- src/providers/posts/posts.ts
+++ src/providers/posts/posts.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Post} from '../../models/post';
+import {User} from '../../models/user';
+
 import {Api} from '../api/api';
 import {Observable} from "rxjs";
 import {Storage} from '@ionic/storage';
@@ -59,6 +61,19 @@ export class Posts {
     return seq
   }
 
+  author(post: Post) {
+
+     return this.storage.get('response').then((val) => {
+        console.log(val);
+        //var author_user = val.user.getUser(post.user_id).subscribe((resp : any) => {
+        //    return resp
+        // });
+        //console.log("Author ", author_user)
+        //return author_user
+    });
+
+    }
+
   like(post: Post) {
 
     return this.storage.get('response').then((val) => {
@@ -68,15 +83,6 @@ export class Posts {
         return this.api.post('user-post-likes', user_id, post.post_id)
     });
 
-  }
-
-  unlike(post: Post) {
-    return this.storage.get('response').then((val) => {
-         console.log(val);
-         var user_id = val.user.user_id;
-         console.log("UnLiking ", post)
-         return this.api.delete('user-post-likes', user_id, post.post_id)
-     });
   }
 
   delete(post: Post) {
