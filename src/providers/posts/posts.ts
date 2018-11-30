8c659ff749194bb306bda3128e75ecdd37d1c67a
ragnar <rahulgovind517@gmail.com>
2018-11-30 05:30:51 -0600

    Search fix

--- src/providers/posts/posts.ts
+++ src/providers/posts/posts.ts
@@ -69,9 +69,6 @@ export class Posts {
         return new Post(post.post_id, post.content, post.user_id, post.title,
           post.description, post.topics, post.likedBy)
       })
-    }).map((res) => {
-      this.posts = res;
-      return res
     }).catch(res => Observable.throw(res));
   }
 
